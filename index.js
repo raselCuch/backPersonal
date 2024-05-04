97ca75db900ded67d5ba1092c5895e48b0d264f4
Rasel <ruselcucho@gmail.com>
2024-05-04 13:19:23 -0500

    Se agrega el modulo cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const conectarDB = require("./config/db.js");
+const cors = require("cors");
 
 const app = express();
 
 // conectar a base de datos
 conectarDB();
+app.use(cors());
 
 app.use(express.json());
 
